72ef30f56033c0f351929eb42ca79bb0abe50632
Ovixy <axentioiovidiu94@gmail.com>
2022-05-23 11:48:38 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/home";
 import About from "./pages/about";
-import { Col, Container, Form, Navbar, Row, Button,Card } from "react-bootstrap";
+import { Col, Container, Form, Navbar, Row, Button } from "react-bootstrap";
 import Movie from "./pages/movie";
 import TopNavigation from "./components/topNavigation.js";
 
@@ -20,19 +20,6 @@ function App() {
         <Route path="/movie/:userId" element={<Movie />} />
         <Route path="*" element={<div>Not found</div>} />
       </Routes>
-
-      <Card className="text-center">
-    <Card.Body>
-      <Card.Title>Made by</Card.Title>
-      <Card.Text>Axentioi Ovidiu Vasile</Card.Text>
-      <Button variant="primary" className="footer-button" href="https://www.linkedin.com/in/ovidiu-axentioi-852297189/">
-        Linkedin
-      </Button>
-      <Button variant="primary" className="footer-button" href="https://github.com/Ovixy">
-        Github
-      </Button>
-    </Card.Body>
-  </Card>  
     </div>
   );
 }
